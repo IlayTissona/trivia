95731df08267c211e314b563f942a91ab2f455ee
DGorgula <gil@dankner.org>
2021-05-01 23:12:10 +0300

    playerStatsBar style fixed

--- client/src/components/PlayerStatsBar.js
+++ client/src/components/PlayerStatsBar.js
@@ -15,21 +15,26 @@ function PlayerStatsBar(props) {
     console.log(avatarUrl);
     return (
         <div id="playerStatsBar">
+            <div id="game-stats">
+                <img className="avatar" src={avatarUrl} alt="AVATAR" />
+                {name}
+                <LivesLast strikes={strikes} />
+            </div>
+            <span id="score"> Score: {score}</span>
+            <svg xmlns="http://www.w3.org/2000/svg" onClick={resetGame} width="32" height="32" fill="currentColor" id="return-arrow" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5z" />
+            </svg>
 
-            <img className="avatar" src={avatarUrl} alt="AVATAR" />
-            {name}
-            {score}
-            <LivesLast strikes={strikes} />
-            <button className="redirect-button" onClick={(e) => {
-                e.preventDefault();
-                console.log("changing the state");
-                player.gameEnded = true
-                dispatch(setPlayer({ ...player }))
-                setTimeout(() => history.push(`/profile/${player.id}`), 4000)
-                return
-            }} >Back to profile</button>
         </div>
     );
+
+    function resetGame(e) {
+        console.log("changing the state");
+        player.gameEnded = true
+        dispatch(setPlayer({ ...player }))
+        setTimeout(() => history.push(`/profile/${player.id}`), 4000)
+        return
+    }
 }
 
 export default PlayerStatsBar;
