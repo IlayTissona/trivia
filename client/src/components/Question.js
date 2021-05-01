e84007cf3c2009d6586ddfc310ec7b65746a2964
GiladVaknin <77235037+GiladVaknin@users.noreply.github.com>
2021-05-01 17:51:17 +0300

    error massage added and leaderBoard table styled

--- client/src/components/Question.js
+++ client/src/components/Question.js
@@ -1,124 +1,132 @@
 import React, { useEffect, useRef } from "react";
 import { Redirect, useHistory } from "react-router-dom";
 import Rank from "./Rank";
-import axios from "../store/axiosWraper"
+import axios from "../store/axiosWraper";
 import "../styles/Question.css";
 import { useDispatch, useSelector } from "react-redux";
 import {
-    postAnswer,
-    setQuestion,
-    setAnswer,
+  postAnswer,
+  setQuestion,
+  setAnswer,
 } from "../store/actions/questionActions";
-import { setPlayer } from '../store/actions/playerActions'
+import { setPlayer } from "../store/actions/playerActions";
 import MiniLoader from "./MiniLoader";
 import Timer from "./Timer";
 
 function Question(props) {
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const player = useSelector((store) => store.player);
-    const question = useSelector(store => store.question)
-    const nextQuestion = useRef({});
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const player = useSelector((store) => store.player);
+  const question = useSelector((store) => store.question);
+  const nextQuestion = useRef({});
 
-    useEffect(() => {
-        if (!player) return <Redirect to="/" />;
-        getNextQuestion()
-            .then((newQuestion) => {
-                nextQuestion.current = newQuestion;
-            })
-            .catch((e) =>
-                getNextQuestion().then((newQuestion) => {
-                    nextQuestion.current = newQuestion;
-                })
-            )
-            .catch((e) => {
-                nextQuestion.current = {
-                    id: "ERROR",
-                    text: "Network Error, can't load next question",
-                };
-            });
-    }, [question.id]);
+  useEffect(() => {
+    if (!player) return <Redirect to="/" />;
+    getNextQuestion()
+      .then((newQuestion) => {
+        nextQuestion.current = newQuestion;
+      })
+      .catch((e) =>
+        getNextQuestion().then((newQuestion) => {
+          nextQuestion.current = newQuestion;
+        })
+      )
+      .catch((e) => {
+        nextQuestion.current = {
+          id: "ERROR",
+          text: "Network Error, can't load next question",
+        };
+      });
+  }, [question.id]);
 
-    useEffect(() => {
-        axios.get(`/game/question/${player.id}`).then((res) => {
-            dispatch(setQuestion(res));
-        });
-    }, []);
-    if (player && player.gameEnded) {
-        return "Game Over"
-    }
-    else if (player && player.strikes >= 3) {
-        player.gameEnded = true;
-        dispatch(setPlayer({ ...player }));
-        setTimeout(() => history.push(`/profile/${player.id}`), 4000)
-    }
-    function goToNextQuestion(next) {
-        dispatch(setQuestion(next));
-        dispatch(setAnswer(null));
-    }
-
-    async function getNextQuestion() {
-        // if (player && player.strikes >= 3) return;
-        return axios
-            .get(`/game/question/${player.id}`)
-            .then((res) => res)
-            .catch((e) => {
-                console.log(`/question/${player.id}`);
-            });
-    }
-    return player && (
-        <>
-            <div id="question">
-                <h1>{question.text}</h1>
-                <div id="options">{createOptions(question.correctAnswer)}</div>
-                {question.timeUp ? "TIME UP" :
-                    question.correctAnswer ? (
-                        <Rank
-                            playerId={player.id}
-                            questionId={question.id}
-                            goToNextQuestion={goToNextQuestion}
-                            nextQuestion={nextQuestion}
-                        />
-                    ) : (
-                        <Timer
-                            goToNextQuestion={goToNextQuestion} nextQuestion={nextQuestion}
-                            timeUp={() => {
-                                dispatch(postAnswer(player.id, question.id, null));
-                            }}
-                        />
-                    )}
-            </div>
-        </>
+  useEffect(() => {
+    axios.get(`/game/question/${player.id}`).then((res) => {
+      dispatch(setQuestion(res));
+    });
+  }, []);
+  if (player && player.gameEnded) {
+    return (
+      <div id="game-over">
+        <h1> GAME OVER ! </h1> <br /> <h3>Try Again... </h3>
+      </div>
     );
+  } else if (player && player.strikes >= 3) {
+    player.gameEnded = true;
+    dispatch(setPlayer({ ...player }));
+    setTimeout(() => history.push(`/profile/${player.id}`), 4000);
+  }
+  function goToNextQuestion(next) {
+    dispatch(setQuestion(next));
+    dispatch(setAnswer(null));
+  }
 
-    function createOptions(correctAnswer) {
-        const { options } = question;
-        const questionOptions = [];
-        for (let i = 0; i < options.length; i++) {
-            questionOptions.push(
-                <div
-                    key={i}
-                    className={"option"
-                        // "option" + correctAnswer === options[i] ? " correct" : " incorrect"
-                    }
-                    onClick={() => {
-                        if (correctAnswer) return;
-                        if (question.timeUp) return;
-                        dispatch(
-                            postAnswer(
-                                player.id,
-                                question.id,
-                                options[i],
-                            )
-                        );
-                    }}
-                >
-                    {correctAnswer === "LOADING" ? <MiniLoader /> : correctAnswer === options[i] ? <strong>{options[i]}</strong> : options[i]}
-                </div>
-            );
-        }
-        return questionOptions;
+  async function getNextQuestion() {
+    // if (player && player.strikes >= 3) return;
+    return axios
+      .get(`/game/question/${player.id}`)
+      .then((res) => res)
+      .catch((e) => {
+        console.log(`/question/${player.id}`);
+      });
+  }
+  return (
+    player && (
+      <>
+        <div id="question">
+          <h1>{question.text}</h1>
+          <div id="options">{createOptions(question.correctAnswer)}</div>
+          {question.timeUp ? (
+            "TIME UP"
+          ) : question.correctAnswer ? (
+            <Rank
+              playerId={player.id}
+              questionId={question.id}
+              goToNextQuestion={goToNextQuestion}
+              nextQuestion={nextQuestion}
+            />
+          ) : (
+            <Timer
+              goToNextQuestion={goToNextQuestion}
+              nextQuestion={nextQuestion}
+              timeUp={() => {
+                dispatch(postAnswer(player.id, question.id, null));
+              }}
+            />
+          )}
+        </div>
+      </>
+    )
+  );
+
+  function createOptions(correctAnswer) {
+    const { options } = question;
+    const questionOptions = [];
+    for (let i = 0; i < options.length; i++) {
+      questionOptions.push(
+        <div
+          key={i}
+          className={
+            "option"
+            // "option" + correctAnswer === options[i] ? " correct" : " incorrect"
+          }
+          onClick={() => {
+            if (correctAnswer) return;
+            if (question.timeUp) return;
+            dispatch(postAnswer(player.id, question.id, options[i]));
+          }}
+        >
+          {correctAnswer === "LOADING" ? (
+            <MiniLoader />
+          ) : correctAnswer === options[i] ? (
+            <strong>{options[i]}</strong>
+          ) : (
+            options[i]
+          )}
+        </div>
+      );
     }
+    return questionOptions;
+  }
 }
 
 export default Question;
--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -1,29 +1,31 @@
 import { useDispatch, useSelector } from "react-redux";
 import React, { useEffect } from "react";
 import { tick } from "../store/actions/timerActions";
-import { setTimeUp } from "../store/actions/questionActions"
+import { setTimeUp } from "../store/actions/questionActions";
 
 export default function Timer({ timeUp, goToNextQuestion, nextQuestion }) {
-    const dispatch = useDispatch();
-    const timer = useSelector((store) => store.timer);
-    useEffect(() => {
-        const interval = setInterval(() => dispatch(tick()), 500);
-        return () => clearInterval(interval);
-    }, []);
+  const dispatch = useDispatch();
+  const timer = useSelector((store) => store.timer);
+  useEffect(() => {
+    const interval = setInterval(() => dispatch(tick()), 500);
+    return () => clearInterval(interval);
+  }, []);
 
-    if (timer.totalTime - timer.timePassed === 0) {
-        dispatch(setTimeUp());
-        setTimeout(
-            () => { timeUp(); goToNextQuestion(nextQuestion.current) }, 5000);
-    }
+  if (timer.totalTime - timer.timePassed === 0) {
+    dispatch(setTimeUp());
+    setTimeout(() => {
+      timeUp();
+      goToNextQuestion(nextQuestion.current);
+    }, 5000);
+  }
 
-    return (
-        <>
-            <div id="timer">
-                {timer.totalTime - timer.timePassed > 0
-                    ? timer.totalTime - timer.timePassed
-                    : "Time Up!"}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div id="timer">
+        {timer.totalTime - timer.timePassed > 0
+          ? timer.totalTime - timer.timePassed
+          : "Time Up!"}
+      </div>
+    </>
+  );
 }
