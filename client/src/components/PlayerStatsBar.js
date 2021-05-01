90d96e2e74e3ec3b54e33edb61675fb27211b9df
DGorgula <gil@dankner.org>
2021-05-01 20:43:18 +0300

    added back to profile button

--- client/src/components/PlayerStatsBar.js
+++ client/src/components/PlayerStatsBar.js
@@ -1,22 +1,34 @@
-import React from 'react';
-import { useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import { Redirect, useHistory } from "react-router-dom";
+import { setPlayer } from "../store/actions/playerActions";
 
 import LivesLast from "./LivesLast";
 import "../styles/PlayerStatsBar.css"
 
 function PlayerStatsBar(props) {
-
-    const player = useSelector((store) => store.player);
+    // const [backToProfile, setBackToProfile] = useState(false)
+    const { player } = useSelector((store) => store);
+    const history = useHistory();
+    const dispatch = useDispatch();
     if (!player) return <Redirect to="/" />;
     const { name, score, strikes, avatarUrl } = player;
     console.log(avatarUrl);
     return (
         <div id="playerStatsBar">
+
             <img className="avatar" src={avatarUrl} alt="AVATAR" />
             {name}
             {score}
             <LivesLast strikes={strikes} />
+            <button className="redirect-button" onClick={(e) => {
+                e.preventDefault();
+                console.log("changing the state");
+                player.gameEnded = true
+                dispatch(setPlayer({ ...player }))
+                setTimeout(() => history.push(`/profile/${player.id}`), 4000)
+                return
+            }} >Back to profile</button>
         </div>
     );
 }
