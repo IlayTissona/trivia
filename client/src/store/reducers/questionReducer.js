b3a8bd1db66e85c6c42fa9346caebe331388ad12
IlayTissona <ilay211@gmail.com>
2021-05-04 23:13:16 +0300

    styling

--- client/src/store/reducers/questionReducer.js
+++ client/src/store/reducers/questionReducer.js
@@ -1,42 +1,44 @@
-
-
 export const initialQuestionState = {
-    id: null,
-    text: null,
-    options: [],
-    correctAnswer: null,
-    timeUp: false
+  id: null,
+  text: null,
+  options: [],
+  correctAnswer: null,
+  timeUp: false,
 };
 
 const questionReducer = (state = initialQuestionState, action) => {
-    if (action.type === "TICK") return state
-    let newState = { ...state };
+  if (action.type === "TICK") return state;
+  let newState = { ...state };
 
-    switch (action.type) {
-        case "SET_NEXT_QUESTION":
-            newState.text = action.question.text;
-            newState.options = action.question.options;
-            newState.id = action.question.id;
-            newState.timeUp = false;
-            break;
+  switch (action.type) {
+    case "SET_NEXT_QUESTION":
+      newState.text = action.question.text;
+      newState.options = action.question.options;
+      newState.id = action.question.id;
+      newState.timeUp = false;
+      newState.correctAnswer = null;
+      break;
 
-        case "TIME_UP":
-            newState.timeUp = true;
-            break;
+    case "TIME_UP":
+      newState.timeUp = true;
+      break;
 
-        case "SET_ANSWER":
+    case "SET_ANSWER":
+      newState.correctAnswer = action.answer;
+      break;
 
-            newState.correctAnswer = action.answer;
-            break;
+    case "SET_ANSWERED":
+      newState.answered = action.answer;
+      break;
 
-        case "LOADING_ANSWER":
-            newState.correctAnswer = "LOADING";
-            break;
-        default:
-            newState = state;
-            break;
-    }
-    return newState;
+    case "LOADING_ANSWER":
+      newState.correctAnswer = "LOADING";
+      break;
+    default:
+      newState = state;
+      break;
+  }
+  return newState;
 };
 
 export default questionReducer;
